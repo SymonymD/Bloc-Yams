ce2a3700bc550a999f350845d1b16cb300e0a851
John Fentner <john.fentner@gmail.com>
2017-07-21 12:46:03 -0400

    Checkpoint 21 Assignment Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,30 +2,28 @@ var animatePoints = function() {
 
             var points = document.getElementsByClassName('point');
 
-            var revealFirstPoint = function() {
-                points[0].style.opacity = 1;
-                points[0].style.transform = "scaleX(1) translateY(0)";
-                points[0].style.msTransform = "scaleX(1) translateY(0)";
-                points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-
-            var revealSecondPoint = function() {
-                points[1].style.opacity = 1;
-                points[1].style.transform = "scaleX(1) translateY(0)";
-                points[1].style.msTransform = "scaleX(1) translateY(0)";
-                points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-
-            var revealThirdPoint = function() {
-                points[2].style.opacity = 1;
-                points[2].style.transform = "scaleX(1) translateY(0)";
-                points[2].style.msTransform = "scaleX(1) translateY(0)";
-                points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-
-            revealFirstPoint();
-            revealSecondPoint();
-            revealThirdPoint();
+            var revealPoint = function(index) {
+              points[index].style.opacity = 1;
+              points[index].style.transform = "scaleX(1) translateY(0)";
+              points[index].style.msTransform = "scaleX(1) translateY(0)";
+              points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+            }
+              for (var i = 0; i < points.length; i++) {
+              revealPoint(i)
+            }
 
         };
         animatePoints();
+
+        var animateHero = function() {
+            var heroContent = document.getElementsByClassName('hero-content');
+
+            var revealHero = function() {
+              heroContent[0].style.opacity = 1;
+              heroContent[0].style.transform = "scaleX(1) translateY(0)";
+              heroContent[0].style.msTransform = "scaleX(1) translateY(0)";
+              heroContent[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+            };
+            revealHero();
+        };
+        animateHero();
